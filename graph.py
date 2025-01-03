8f6399392a51ac07caa09d4968d5486defc7f9a1
MrBinit <sapkotabinit2002@gmail.com>
2025-01-03 11:03:51 +0545

    created hetero graph from csv

---
+++ graph.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import torch
+from torch_geometric.data import HeteroData
+
+def create_hetero_graph_from_csv(file_path):
+    data = pd.read_csv(file_path)
+    hetero_data = HeteroData()
+    
+    # need to ask paras for transcation ids.
+
+    # Extract unique account and transaction IDs
+    account_ids = pd.unique(data[['From_Account_id', 'To_Account_id']].values.ravel('K'))
+    transaction_ids = data['Transaction_ID'].unique()
+    # mappings for accounts and transactions
+    account_mapping = {id: idx for idx, id in enumerate(account_ids)}
+    transaction_mapping = {id: idx for idx, id in enumerate(transaction_ids)}
+
+    # Adding 'account' and 'transcation' nodes with placeholder features
+    hetero_data['account'].x = torch.zeros((len(account_ids), 1))
+    transaction_features = data[['amount', 'Year', 'Month', 'Day', 'Hour', 'Minute',
+                                 'Transaction_Count', 'Transaction_Sum', 'Average_Transaction_Amount']].values
+    hetero_data['transaction'].x = torch.tensor(transaction_features, dtype=torch.float)
+
+    # transaction initiated and received edges.
+    initiates_src = [account_mapping[id] for id in data['From_Account_id']]
+    initiates_dst = [transaction_mapping[id] for id in data['Transaction_ID']]
+    hetero_data['account', 'initiates', 'transaction'].edge_index = torch.tensor([initiates_src, initiates_dst], dtype=torch.long)
+
+    receives_src = [transaction_mapping[id] for id in data['Transaction_ID']]
+    receives_dst = [account_mapping[id] for id in data['To_Account_id']]
+    hetero_data['transaction', 'receives', 'account'].edge_index = torch.tensor([receives_src, receives_dst], dtype=torch.long)
+
+    return hetero_data
+
+
+if __name__ == "__main__":
+    hetero_data = create_hetero_graph_from_csv('/home/binit/Anti-Money-Laundrying/data/test.csv')
+    print(hetero_data)
